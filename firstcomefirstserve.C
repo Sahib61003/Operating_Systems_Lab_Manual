4b2294d3b2f634232121a6cb17e833fa0ae05dad
Sahib61003m <sahib61003@gmail.com>
2023-10-25 00:34:33 +0530

    Added exp 1

---
+++ firstcomefirstserve.C
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[]) {
+    wt[0] = 0;
+
+    for (int i = 1; i < n; i++)
+        wt[i] = bt[i - 1] + wt[i - 1];
+}
+
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++)
+        tat[i] = bt[i] + wt[i];
+}
+
+void findavgTime(int processes[], int n, int bt[]) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    findWaitingTime(processes, n, bt, wt);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    printf("--------------------------------------------------------\n");
+    printf("| Process | Burst Time | Waiting Time | Turnaround Time |\n");
+    printf("--------------------------------------------------------\n");
+
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("|    %2d   |     %3d     |      %3d     |        %3d       |\n", processes[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("--------------------------------------------------------\n");
+
+    float s = (float)total_wt / (float)n;
+    float t = (float)total_tat / (float)n;
+    printf("Average waiting time = %f\n", s);
+    printf("Average turnaround time = %f\n", t);
+}
+
+int main() {
+    int processes[] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof processes / sizeof processes[0];
+    int burst_time[] = {10, 5, 8, 6, 9, 12};
+    findavgTime(processes, n, burst_time);
+    return 0;
+}
